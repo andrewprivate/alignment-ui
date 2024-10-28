cfe470bb5538afcdef9adfe939db44703a237515
Andrew S <32175943+andrewprivate@users.noreply.github.com>
2024-10-28 17:00:29 -0500

    Update

--- src/app.py
+++ src/app.py
@@ -4,7 +4,7 @@ import os
 class App:
     def __init__(self):
         self.name = 'App'
-        web_dir = os.path.join(os.path.dirname(__file__), 'web')
+        web_dir = os.path.join(os.path.dirname(__file__), '../web')
         eel.init(web_dir)
 
     def run(self):
